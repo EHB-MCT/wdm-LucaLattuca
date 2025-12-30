feb444023824b24d7cb4f711eceefa728c760d6d
LucaLattuca <luca.lattuca@student.ehb.be>
2025-12-30 19:53:50 +0100

    Update api.php
    import gameController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
